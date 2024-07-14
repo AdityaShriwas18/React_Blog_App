b1634c64ac86d6394e18e6e9c18f7f4418211e35
Aditya Shriwas <161008667+AdityaShriwas18@users.noreply.github.com>
2024-07-14 01:10:03 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+build: {
+  rollupOptions: {
+    external: ['/node_modules/.vite/deps/appwrite.js']
+  }
+}
